52decbf34db7af1258c5923b6e70db48e6bdd616
Alecsandro Bez Stolk <alecsandro.stolk@gmail.com>
2020-04-06 10:25:37 -0300

    Autenticação e autorização;

--- src/main/java/br/com/alecsandro/estudos/config/SecurityConfig.java
+++ src/main/java/br/com/alecsandro/estudos/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-//                .anyRequest().authenticated()
+                .anyRequest().authenticated()
 //                .antMatchers("/*/usuario/**").hasAnyRole("USER")
 //                .antMatchers("/*/admin/**").hasAnyRole("ADMIN")
                 .and()
